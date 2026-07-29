b6f799b79d6dc0efc80f2f686bff43e1af64e855
Manasvi Goyal <mg.manasvi@gmail.com>
2026-07-29 10:39:35 -0700

    add missing f16c flags
    Signed-off-by: Manasvi Goyal <mg.manasvi@gmail.com>

--- jni/cmake/init-simd.cmake
+++ jni/cmake/init-simd.cmake
@@ -99,7 +99,7 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" AND AVX512_ENABLED)
     # -mavx512f	: 512-bit Registers & FMA. Ex: __m512, _mm512_fmadd_ps
     # -mavx512bw : 16-bit Masking. Ex: __mmask16, mask generation
     # -mavx512vl : 256-bit "Sub-vector" support. Ex: _mm256_maskz_loadu_epi16
-    set(CMAKE_REQUIRED_FLAGS "-mavx512f -mavx512vl -mavx512bw")
+    set(CMAKE_REQUIRED_FLAGS "-mavx512f -mavx512vl -mavx512bw -mf16c")
     check_cxx_source_compiles("
         #include <immintrin.h>
         int main() {
@@ -113,7 +113,7 @@ elseif(${CMAKE_SYSTEM_NAME} STREQUAL "Linux" AND AVX512_ENABLED)
     if(HAVE_AVX512_COMPILER)
         set(KNN_HAVE_AVX512 ON)
         set(SIMD_OPT_LEVEL "avx512") # Keep optimization level as avx512 to improve performance on Linux. This is not present on mac systems, and presently not supported on Windows OS.
-        set(SIMD_FLAGS -mavx512f -mavx512vl -mavx512bw)
+        set(SIMD_FLAGS -mavx512f -mavx512vl -mavx512bw -mf16c)
         add_definitions(-DKNN_HAVE_AVX512)
         message(STATUS "[SIMD] AVX512 + F16C supported by compiler.")
     else()
