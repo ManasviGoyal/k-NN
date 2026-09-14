07e96396f12e9e377dc0b5d6218980462cd7d4c1
Manasvi Goyal <mg.manasvi@gmail.com>
2026-09-14 11:27:27 -0700

    use cosine optimization for half_float
    Signed-off-by: Manasvi Goyal <mg.manasvi@gmail.com>

--- src/main/java/org/opensearch/knn/index/engine/lucene/LuceneHNSWMethod.java
+++ src/main/java/org/opensearch/knn/index/engine/lucene/LuceneHNSWMethod.java
@@ -9,7 +9,6 @@ import com.google.common.collect.ImmutableSet;
 import org.opensearch.knn.index.KNNSettings;
 import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.VectorDataType;
-import org.opensearch.knn.index.engine.AbstractKNNMethod;
 import org.opensearch.knn.index.engine.Encoder;
 import org.opensearch.knn.index.engine.MethodComponent;
 import org.opensearch.knn.index.engine.Parameter;
@@ -28,7 +27,7 @@ import static org.opensearch.knn.common.KNNConstants.METHOD_PARAMETER_M;
 /**
  * Lucene HNSW implementation
  */
-public class LuceneHNSWMethod extends AbstractKNNMethod {
+public class LuceneHNSWMethod extends AbstractLuceneMethod {
 
     private static final Set<VectorDataType> SUPPORTED_DATA_TYPES = ImmutableSet.of(
         VectorDataType.FLOAT,
